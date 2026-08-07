19f728dc711b23e96ac0ed0527b3e1c4c75e2ea9
CHRISTOPH <mundotravelspa@gmail.com>
2026-08-07 02:31:38 -0400

    Sprint 225.1 - League Playoffs Complete

---
+++ components/live/playoffs/types.ts
@@ -0,0 +1,39 @@
+﻿export type PlayoffRound =
+  | "ROUND_OF_16"
+  | "QUARTERFINAL"
+  | "SEMIFINAL"
+  | "FINAL";
+
+export interface PlayoffCompetitor {
+  creatorId: string;
+  creatorName: string;
+
+  league: string;
+
+  seed: number;
+
+  score: number;
+}
+
+export interface PlayoffMatch {
+  id: string;
+
+  round: PlayoffRound;
+
+  left: PlayoffCompetitor;
+  right: PlayoffCompetitor;
+
+  winnerId: string | null;
+
+  finished: boolean;
+}
+
+export interface LeaguePlayoffState {
+  season: number;
+
+  round: PlayoffRound;
+
+  matches: PlayoffMatch[];
+
+  champion: PlayoffCompetitor | null;
+}
