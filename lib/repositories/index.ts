f877de2c17d97828b56e48f920cffb54cc62e3ae
CHRISTOPH <mundotravelspa@gmail.com>
2026-08-20 16:27:14 -0400

    refactor(notifications): remove unused repository

--- lib/repositories/index.ts
+++ lib/repositories/index.ts
@@ -1,5 +1,4 @@
-﻿export * from "./notification.repository";
-export * from "./repository";
+﻿export * from "./repository";
 export * from "./support.repository";
 export * from "./user.repository";
 export * from "./wallet.repository";
