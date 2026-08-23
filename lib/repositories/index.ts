4bae7ddce50ec09a7f6d21e077f0bd75b84e7f94
CHRISTOPH <mundotravelspa@gmail.com>
2026-08-23 02:18:27 -0400

    Remove obsolete repository placeholders

--- lib/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-﻿export * from "./repository";
-export * from "./support.repository";
-export * from "./user.repository";
-export * from "./wallet.repository";
--- lib/repositories/repository.ts
+++
@@ -1,9 +0,0 @@
-﻿import type { DataResult } from "@/lib/core";
-
-export interface Repository<T, Create = Partial<T>, Update = Partial<T>> {
-  findAll(): Promise<DataResult<T[]>>;
-  findById(id: string): Promise<DataResult<T>>;
-  create(data: Create): Promise<DataResult<T>>;
-  update(id: string, data: Update): Promise<DataResult<T>>;
-  remove(id: string): Promise<DataResult<boolean>>;
-}
